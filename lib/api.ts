dff97392c4ffdb6d529d4e8c477876bc611b8759
create-newspulse <newspulse.ads@gmail.com>
2025-05-18 10:53:42 +0530

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ This MUST be localhost:5000/api
+  baseURL: "http://localhost:5000/api", // ✅ this is key
 });
 
 export default api;
