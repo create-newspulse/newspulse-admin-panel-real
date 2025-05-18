9d3065ace413c19a248ab447c174b2344abac962
create-newspulse <newspulse.ads@gmail.com>
2025-05-18 13:41:42 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>📰 Welcome to News Pulse Admin Panel</h1>
+      <p style={{ color: '#0070f3' }}>Powered by Vite + React</p>
+    </div>
+  );
+}
