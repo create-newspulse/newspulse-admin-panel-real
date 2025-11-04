9258103799e4638c498ef7e58a1c5abe539c5a3b
create-newspulse <newspulse.ads@gmail.com>
2025-11-04 21:19:34 +0530

    build: explicitly import ./App.tsx in main to avoid picking stale App.js on CI

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
+// Explicit extension to prefer the TSX source over any stray JS artifacts in the repo
+import App from "./App.tsx";
 import "./index.css";
 
 // 🧠 Global Providers
